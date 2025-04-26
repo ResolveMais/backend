5e0454c341dca148e84c01e0864447c69b447f07
marcosjunior00 <marcosantonio.jose01@gmail.com>
2025-04-25 23:49:19 -0300

    feat: insert company model

---
+++ app/models/company.model.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, Sequelize) => {
+	const Company = sequelize.define('company', {
+		id: {
+			type: Sequelize.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		name: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			unique: true,
+		},
+        description: {
+            type: Sequelize.STRING,
+        },
+        cnpj: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+        },
+	});
+
+	return Company;
+};
