63ff8d639aaf7109ef57eabfa4a7ef7ab52e35d4
marcosjunior00 <marcosantonio.jose01@gmail.com>
2026-03-18 10:35:06 -0300

    feat: remove unnecessary variable assignment for models import

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const db = require('./app/models');
+require("./app/models");
 
 routesController(app);
 
