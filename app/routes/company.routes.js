e1944b80afaea0cc4447101d64330a52a3222e94
marcosjunior00 <marcosantonio.jose01@gmail.com>
2026-03-23 18:19:54 -0300

    feat: add updateMyCompanyProfile and updateMyCompanyEmployee routes

--- app/routes/company.routes.js
+++ app/routes/company.routes.js
@@ -7,11 +7,13 @@ const CompanyController = require('../controllers/company.controller.js');
 
 router.get("/all", CompanyController.getAll);
 router.get("/my-company/admins", authMiddleware, CompanyController.getMyCompanyAdmins);
+router.patch("/my-company/profile", authMiddleware, CompanyController.updateMyCompanyProfile);
 router.post("/my-company/admins", authMiddleware, CompanyController.addMyCompanyAdmin);
 router.patch("/my-company/admins/:adminUserId/primary", authMiddleware, CompanyController.setMyCompanyPrimaryAdmin);
 router.delete("/my-company/admins/:adminUserId", authMiddleware, CompanyController.removeMyCompanyAdmin);
 router.get("/my-company/employees", authMiddleware, CompanyController.getMyCompanyEmployees);
 router.post("/my-company/employees", authMiddleware, CompanyController.addMyCompanyEmployee);
+router.patch("/my-company/employees/:employeeUserId", authMiddleware, CompanyController.updateMyCompanyEmployee);
 router.delete("/my-company/employees/:employeeUserId", authMiddleware, CompanyController.removeMyCompanyEmployee);
 
 module.exports = { alias: "/api/companies", router };
